cd85a6ac285f47ce70d5913c0aa3c004e13edfc6
James Dolan <jad0001326@gmail.com>
2025-02-06 12:58:01 +0000

    js updated

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// Open modal
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+// Close modal
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+// Close modal when clicking outside the modal content
+window.onclick = function(event) {
+    const modals = document.querySelectorAll(".modal");
+    modals.forEach(modal => {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+};
